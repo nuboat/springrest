fdff6a989c4d18df23c04035577a0e572d13954c
nuboat <nuboat@gmail.com>
2015-08-01 01:13:50 +0700

    Initail Project

---
+++ src/main/java/thjug/springboot/Application.java
@@ -0,0 +1,19 @@
+package thjug.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
