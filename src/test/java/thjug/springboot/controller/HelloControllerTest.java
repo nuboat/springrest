50b509c42ed6988684a1a480b750c1907ec7e671
nuboat <nuboat@gmail.com>
2015-08-01 02:12:37 +0700

    Mock MVC

---
+++ src/test/java/thjug/springboot/controller/HelloControllerTest.java
@@ -0,0 +1,45 @@
+package thjug.springboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import thjug.springboot.Application;
+
+@Slf4j
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@ContextConfiguration(classes = MockServletContext.class)
+public class HelloControllerTest {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void testHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+            .get("/hello")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().string(
+                is("Hello World !")));
+    }
+
+}
